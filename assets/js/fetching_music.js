45e203dc021c51eb8def00addaccf1ede11d346a
Asuna Masuda <asuna.fitz@gmail.com>
2020-03-19 19:21:30 +0000

    Add Youtube video section. Fix layouts in several sections in JQuery html() method.

--- assets/js/fetching_music.js
+++ assets/js/fetching_music.js
@@ -1,6 +1,6 @@
 var musix_api_url = 'https://api.musixmatch.com/ws/1.1/'
-var client_id_misix = 'API KEY'
-deepai.setApiKey('API KEY');
+var client_id_misix = '0d080d92b5b22fb4c5e349ed087defa8'
+deepai.setApiKey('8b511c81-08a3-4db2-9d6e-ced35b2d1d70');
 
 // Search Section
 $("input[id='artist_search']").on("click", function () {
@@ -22,16 +22,16 @@ function fetchArtistInformation() {
     let artist = $("#artist_id").val();
     $("#search_result_body").html('');
     $.getJSON(musix_api_url + 'track.search?format=jsonp&callback=?&q_artist='
-        + artist + '&f_has_lyrics=true&s_track_rating=desc&page_size=5&apikey=' + client_id_misix,
+        + artist + '&f_has_lyrics=true&s_track_rating=desc&page_size=10&apikey=' + client_id_misix,
         function (data) {
             if (data["message"]["header"]["status_code"] !== 200) {
                 $("#search_result_body").html(`<p>Error occured. Please try it again.</p>`)
             }
             else {
-                $("#search_result_body").html(`<p>Choose a song to get the lyrics.</p>`)
+                $("#search_result_body1").html(`<h5>Choose a song to get the lyrics.</h5>`)
                 $(data.message.body.track_list).each(function () {
                     let track_name = this.track.track_name;
-                    $('<ul name="track" class="song_name">' + track_name + '</ul>').appendTo("#search_result_body");
+                    $('<ul name="track" class="song_name">' + track_name + '</ul>').appendTo("#search_result_body2");
                 })
             }
         })
@@ -40,19 +40,22 @@ function fetchArtistInformation() {
 // Retrieving Lyrics
 $('body').on('click', '.song_name', function () {
     let i = $("ul.song_name").index(this);
-    let song = document.getElementsByName("track")[i].textContent;
     let artist = $("#artist_id").val();
+    var song = document.getElementsByName("track")[i].textContent;
     $.getJSON(musix_api_url + 'matcher.lyrics.get?format=jsonp&callback=?&q_track='
         + song + '&q_artist=' + artist + '&apikey=' + client_id_misix,
         function (data) {
-            $("#lyrics_body").html(`<p id="lyrics">` + data["message"]["body"]["lyrics"]["lyrics_body"].split("...")[0] + 
-            `</p><button onclick="SentimentAnalysis()">Sentiment Analysis</button>`)
+        var lyrics_body = data["message"]["body"]["lyrics"]["lyrics_body"].split("...")[0];
+        var cleaned = lyrics_body.replace(/\r?\n/g, "<br />");    
+            console.log(cleaned);
+            $("#show_lyrics").html(`<div id="lyrics">` + cleaned +
+                `</div><button onclick="SentimentAnalysis()">Sentiment Analysis</button>`)
         })
 })
 
 // Sentiment Analysis
 function SentimentAnalysis() {
-(async function () {
+    (async function () {
         var resp = await deepai.callStandardApi("sentiment-analysis", {
             text: document.getElementById('lyrics').textContent,
         });
@@ -60,29 +63,58 @@ function SentimentAnalysis() {
     })()
 }
 
-// Related Artists & Videos
+// Recommended Artists
 function getArtistID() {
     let defer = new $.Deferred;
     let artist = $("#artist_id").val();
-    $.getJSON(musix_api_url + '/artist.search?format=jsonp&callback=?&q_artist=' + artist + 
-    '&page_size=1&apikey=' + client_id_misix,
+    $.getJSON(musix_api_url + '/artist.search?format=jsonp&callback=?&q_artist=' + artist +
+        '&page_size=1&apikey=' + client_id_misix,
         function (data) {
             let artist_id = data["message"]["body"]["artist_list"][0]["artist"]["artist_id"]
             defer.resolve(artist_id);
         });
-        return defer.promise()
+    return defer.promise()
 }
 
-function getRelatedArtists(id){
-    $.getJSON(musix_api_url + 'artist.related.get?format=jsonp&callback=?&artist_id=' + id + 
-    '&page_size=3&page=1&apikey=' + client_id_misix,
-    function(data){
-        $(data.message.body.artist_list).each(function(){
-            let related_artist = this.artist.artist_name;
-            $('<ul>' + related_artist + '</ul>').appendTo("related_artist")
-        })
-    }
-    )}
- 
+function getRelatedArtists(id) {
+    $.getJSON(musix_api_url + 'artist.related.get?format=jsonp&callback=?&artist_id=' + id +
+        '&page_size=3&page=1&apikey=' + client_id_misix,
+        function (data) {
+            if (data.message.body.artist_list.length !== 0)
+                $("#related_artist").html(`<h5>Recommended Artists</h5>`)
+            $(data.message.body.artist_list).each(function () {
+                let related_artist = this.artist.artist_name;
+                $('<ul>' + related_artist + '</ul>').appendTo("#related_artist");
+            })
+        }
+    )
+}
+
 function fetchSongInformation() {
-    getArtistID().then(function (artist_id) {getRelatedArtists(artist_id)})}
+    getArtistID().then(function (artist_id) { getRelatedArtists(artist_id) })
+}
+
+// Related Video
+function googleApiClientReady() {
+    gapi.client.setApiKey('AIzaSyDW6KJ1DWJykloq__vRMEXa3Hg4LxqlLAQ');
+    gapi.client.load('youtube', 'v3', function () {
+        search();
+    });
+}
+
+function search() {
+    var query = $("#artist_id").val();
+    var request = gapi.client.youtube.search.list({
+        q : query,
+        part: 'id',
+        type: 'video',
+        maxResults: 1
+    });
+
+    request.execute(function (response) {
+        var video_id = response.result["items"][0]["id"]["videoId"];
+        $("#video-window").html(`<h5>Watch the most popular video of this artist</h5><iframe width="100%" height="100%" src="https://www.youtube.com/embed/` + video_id +
+        `" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+        console.log(video_id);
+    })
+}
