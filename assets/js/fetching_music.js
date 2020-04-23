8563ec1d98e669ec2bd11212e8b47e3b9649e5cd
Asuna Masuda <asuna.fitz@gmail.com>
2020-04-23 22:23:03 +0000

    amend fetching_music.js file where it got warning by validation tool.

--- assets/js/fetching_music.js
+++ assets/js/fetching_music.js
@@ -3,19 +3,18 @@ const clientIdMisix = '0d080d92b5b22fb4c5e349ed087defa8'
 const youtubeAPI = 'AIzaSyAdXwgHUraNeXJ22jYuc2WFTMvRRo89CQE'
 const lastFMAPI = 'd7b03ea0c367902fbe0146692fa6b15a'
 
-// Auto Complete Artist Name
 function getListOfArtists() {
-    let defer = new $.Deferred;
+    let defer = new $.Deferred();
     $.getJSON(musixApiUrl + 'artist.search?format=jsonp&callback=?&page_size=10&q_artist=' + $("#artist").val() + '&apikey=' + clientIdMisix,
         function (data) {
             let listCan = [];
             let artistList = data.message.body.artist_list;
             for (var i = 0; i < artistList.length; i++) {
-                var candidate = artistList[i]["artist"]["artist_name"];
+                var candidate = artistList[i].artist.artist_name;
                 listCan.push(candidate);
                 defer.resolve(listCan);
             }
-        })
+        });
     return defer.promise();
 }
 
@@ -26,11 +25,12 @@ function showListOfArtists(list) {
         source: list,
         select: selectItem,
         minLength: 1
-    })
+    });
 }
 
 function getListOfAutocomplete() {
-    getListOfArtists().then(function (list_artist) { showListOfArtists(list_artist) })
+    getListOfArtists().then(function (list_artist) { showListOfArtists(list_artist);
+});
 }
 
 
@@ -38,19 +38,19 @@ function getListOfAutocomplete() {
 var pageNum = 1;
 function fetchArtistInformation() {
     $("#search_result_body2").html('');
-    getSongList()
+    getSongList();
 }
 
 function getSongList() {
     var artist = $("#artist").val();
     $.getJSON(musixApiUrl + 'track.search?format=jsonp&callback=?&q_artist='
-        + artist + '&f_has_lyrics=true&s_track_rating=desc&page_size=15&page=' + pageNum + '&apikey=' + clientIdMisix,
++ artist + '&f_has_lyrics=true&s_track_rating=desc&page_size=15&page=' + pageNum + '&apikey=' + clientIdMisix,
         function (data) {
-            if (data["message"]["header"]["status_code"] !== 200) {
-                $("#search_result_body1").html(`<p>Error occured. Please try it again.</p>`)
+            if (data.message.header.status_code !== 200) {
+                $("#search_result_body1").html(`<p>Error occured. Please try it again.</p>`);
             }
-            else if (data["message"]["body"]["track_list"].length == 0) {
-                $("#search_result_body1").html(`<p>There is no data with the artist name in musixmatch database. <br>Please try a different name.</p>`)
+            else if (data.message.body.track_list.length == 0) {
+                $("#search_result_body1").html(`<p>There is no data with the artist name in musixmatch database. <br>Please try a different name.</p>`);
             }
             else {
                 $("#search_result_body1").html(`<h5>Choose a song to get the lyrics & video.</h5>`);
@@ -61,7 +61,7 @@ function getSongList() {
                 $('<button onclick="loadmore()" id="hide" class="btn btn-link">Load More</button>').appendTo("#search_result_body2");
             }
         }
-    )
+    );
 }
 
 function loadmore() {
@@ -73,7 +73,7 @@ function loadmore() {
 
 // Retrieving Lyrics & Get Youtube video
 function getLyrics() {
-    let defer = new $.Deferred;
+    let defer = new $.Deferred();
     $('body').on('click', '.song_name', function () {
         let i = $("li.song_name").index(this);
         let artist = $("#artist").val();
@@ -83,16 +83,15 @@ function getLyrics() {
             + song + '&q_artist=' + artist + '&apikey=' + clientIdMisix,
             function (data) {
                 if (data.message.header.status_code == 200) {
-                    console.log(data)
-                    let lyrics_body = data["message"]["body"]["lyrics"]["lyrics_body"].split("...")[0];
+                    let lyrics_body = data.message.body.lyrics.lyrics_body.split("...")[0];
                     let cleaned = lyrics_body.replace(/\r?\n/g, "<br />");
-                    $("#show_lyrics").html(`<h4>` + song + `</h4><div id="lyrics">` + cleaned + `</div>`)
+                    $("#show_lyrics").html(`<h4>` + song + `</h4><div id="lyrics">` + cleaned + `</div>`);
                 }
                 else if (data.message.header.status_code == 404) {
-                    $("#show_lyrics").html(`<h5>Lyrics data was not found in musixmatch database. Please choose a different song or search another artist.</h5>`)
+                    $("#show_lyrics").html(`<h5>Lyrics data was not found in musixmatch database. Please choose a different song or search another artist.</h5>`);
                 }
                 else {
-                    $("#show_lyrics").html(`<h5>Error occured. Please try it again or choose a different song.</h5>`)
+                    $("#show_lyrics").html(`<h5>Error occured. Please try it again or choose a different song.</h5>`);
                 }
             });
     });
@@ -117,29 +116,29 @@ function search(songChosen) {
 
     request.execute(function (response) {
         $("#video-window").html('');
-        let videoId = response.result["items"][0]["id"]["videoId"];
+        let videoId = response.result.items[0].id.videoId;
         $("#video-window").html(`<h5>Watch the most related video.</h5><iframe width="100%" height="100%" src="https://www.youtube.com/embed/` + videoId +
-            `" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+            `" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
         console.log(videoId);
-    })
+    });
 }
 
 function fetchLyricsVideoInformation() {
-    getLyrics().then(function (song) { googleApiClientReady(song) })
+    getLyrics().then(function (song) { googleApiClientReady(song) });
 }
 
 
 // Recommended Artists
 function getArtistID() {
-    let defer = new $.Deferred;
+    let defer = new $.Deferred();
     let artist = $("#artist").val();
     $.getJSON(musixApiUrl + '/artist.search?format=jsonp&callback=?&q_artist=' + artist +
         '&page_size=1&apikey=' + clientIdMisix,
         function (data) {
-            let artistId = data["message"]["body"]["artist_list"][0]["artist"]["artist_id"]
+            let artistId = data.message.body.artist_list[0].artist.artist_id;
             defer.resolve(artistId);
         });
-    return defer.promise()
+    return defer.promise();
 }
 
 function getRelatedArtists(id) {
@@ -151,17 +150,18 @@ function getRelatedArtists(id) {
                 $(data.message.body.artist_list).each(function () {
                     let related_artist = this.artist.artist_name;
                     $('<ul>' + related_artist + '</ul>').appendTo("#related_artist");
-                })
+                });
             }
             else {
                 $("#related_artist").html(`<h5>Recommended Artists</h5>
                 <p>Data not found in musixmatch database.</p>`);
             }
-        })
+        });
 }
 
 function fetchRelatedSongInformation() {
-    getArtistID().then(function (artistId) { getRelatedArtists(artistId) })
+    getArtistID().then(function (artistId) { 
+      getRelatedArtists(artistId); });
 }
 
 function getInfoOfArtistLastFM() {
@@ -173,12 +173,12 @@ function getInfoOfArtistLastFM() {
                 $("#artist_info").html('<h5>Artist Information from Last.FM</h5><p>There is no artist data about ' + artist + ' in  Last.FM</p>');
             }
             else {
-                let artistUrl = data["artist"]["url"];
-                let artistListeners = data["artist"]["stats"]["listeners"];
-                let artistPlaycounts = data["artist"]["stats"]["playcount"];
+                let artistUrl = data.artist.url;
+                let artistListeners = data.artist.stats.listeners;
+                let artistPlaycounts = data.artist.stats.playcount;
                 $("#artist_info").html('<h5>Artist Information from Last.FM</h5><div><div><p>In last.FM, there are... <br><b>' + artistListeners +
                     ' </b>listeners<br><b>' + artistPlaycounts + ' </b>playcounts</p><a href="' + artistUrl + '" target=”_blank”>Visit Artist Page in last.FM</a>');
             }
         }
-    )
+    );
 }
